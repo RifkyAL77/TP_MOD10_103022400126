9ceb3a7c436fc09185eea248a03134ae864a883e
RifkyAL77 <rifkyalbuchorilearning@gmail.com>
2026-05-03 15:36:12 +0700

    Menambahkan API Film

---
+++ TP_MODUL10_103022400126/Controllers/FilmController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TP_MODUL10_103022400126;
+
+namespace TP_MODUL10_103022400126.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmController : ControllerBase
+    {
+        private static List<Film> _films = new List<Film>
+        {
+            new Film { Judul = "Inception", Sutradara = "Christopher Nolan", Tahun = "2010", Genre = "Sci-Fi", Rating = "9.0" },
+            new Film { Judul = "Interstellar", Sutradara = "Christopher Nolan", Tahun = "2014", Genre = "Sci-Fi", Rating = "8.7" },
+            new Film { Judul = "Parasite", Sutradara = "Bong Joon-ho", Tahun = "2019", Genre = "Thriller", Rating = "8.6" }
+        };
+
+        [HttpGet]
+        public IEnumerable<Film> Get()
+        {
+            return _films;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Film> Get(int id)
+        {
+            if (id < 0 || id >= _films.Count)
+            {
+                return NotFound();
+            }
+            return _films[id];
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Film film)
+        {
+            _films.Add(film);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (id < 0 || id >= _films.Count)
+            {
+                return NotFound();
+            }
+            _films.RemoveAt(id);
+            return Ok();
+        }
+    }
+}
